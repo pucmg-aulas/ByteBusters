db4fd767ab71d02a1bce9bdf088a8fd5b998012d
Rafael Lopes <145872285+RafaelLopes1810@users.noreply.github.com>
2024-09-22 20:29:54 -0300

    Update classe Main

--- codigo/estacionamento/Main.java
+++ codigo/estacionamento/Main.java
@@ -1,23 +1,43 @@
 package estacionamento;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
+        Estacionamento estacionamento1 = new Estacionamento("Estacionamento A", "Rua A");
+        estacionamento1.mostrarEstacionamento();
+
+        estacionamento1.adicionarVaga("A", 1);
+        estacionamento1.adicionarVaga("A", 2);
+        estacionamento1.adicionarVaga("B", 1);
+        estacionamento1.adicionarVaga("B", 2);
+        estacionamento1.mostrarVagas();
         
-        Estacionamento a = new Estacionamento("Estacionamento A", "Rua A");
-        a.mostrarEstacionamento();
-        a.mostrarVagas();
+        Cliente cliente1 = new Cliente("João");
+        Veiculo veiculo1 = new Veiculo("AAA-1111");
+        Veiculo veiculo2 = new Veiculo("BBB-2222");
         
-        a.adicionarVaga("A", 1);
-        a.adicionarVaga("A", 2);
-        a.adicionarVaga("B", 1);
-        a.adicionarVaga("B", 2);
-        a.mostrarVagas();
+        cliente1.registrarVeiculo(veiculo1);
+        cliente1.registrarVeiculo(veiculo2);
+        cliente1.mostraVeiculosDoCliente();
         
-        a.getVagas().get(0).ocupaVaga();
-        a.mostrarEstacionamento();
-        a.mostrarVagas();
+        Vaga vagaDisponivel = estacionamento1.getVagas().get(0);
+        veiculo1.associarVaga(vagaDisponivel);
+        veiculo1.mostrarVeiculo();
+      
+        estacionamento1.mostrarVagas();
         
+        // Criando uma cobrança para o veículo 1 com a horaChegada atual e horaSaida (1hr e 30min depois) na própria instância
+        Cobranca cobrancaVeiculo1 = new Cobranca(veiculo1, new Date(), new Date(new Date().getTime() + (90 * 60 * 1000)));
+
+        cobrancaVeiculo1.mostrarCobranca();
+
+        cobrancaVeiculo1.efetuarPagamento();
+        
+        veiculo1.mostrarVeiculo();
+        estacionamento1.mostrarVagas();
     }
-    
 }
+
+
