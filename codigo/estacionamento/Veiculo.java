578bc02ccdcf9511e9774067d8de0ad2bfcc26a9
Rafael Lopes <145872285+RafaelLopes1810@users.noreply.github.com>
2024-09-22 20:31:07 -0300

    Update classe Veiculo

--- codigo/estacionamento/Veiculo.java
+++ codigo/estacionamento/Veiculo.java
@@ -1,5 +1,41 @@
 package estacionamento;
 
 public class Veiculo {
+    private final String placa;
+    private Vaga vagaAtual;
+
+    public Veiculo(String placa) {
+        this.placa = placa;
+        this.vagaAtual = null;
+    }
+
+    public void associarVaga(Vaga vaga) {
+        this.vagaAtual = vaga;
+        vaga.ocupaVaga(); 
+    }
+
+    public void removeVaga() {
+        if (this.vagaAtual != null) {
+            this.vagaAtual.desocupaVaga();
+            this.vagaAtual = null;
+        }
+    }
+
+    public void mostrarVeiculo() {
+        System.out.println("\nVeículo placa: " + placa);
+        if (vagaAtual != null) {
+            System.out.println("Estacionado na vaga: " + vagaAtual.getId());
+        } else {
+            System.out.println("O veículo não está estacionado.");
+        }
+    }
     
+    public String getPlaca() {
+        return placa;
+    }
+
+    public Vaga getVagaAtual() {
+        return vagaAtual;
+    }
 }
+
