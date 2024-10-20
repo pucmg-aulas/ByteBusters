c3e28d2dce4498bc542edc51331e39fdf44c9b8f
Kayque Allan <164039275+kayqueallan@users.noreply.github.com>
2024-10-20 18:39:06 -0300

    Update Vip.java

--- codigo/src/estacionamento/Vip.java
+++ codigo/src/estacionamento/Vip.java
@@ -1,9 +1,13 @@
 package estacionamento;
 
 public class Vip extends Vaga {
+    public Vip(String fila, int numero) {
+        super(fila, numero);
+        this.fatorPreco = 1.5; // Exemplo de fator de preço
+    }
 
-    public Vip(String fila, int numVaga) {
-        super(fila, numVaga, "VIP");
-        this.fatorPreco = 1.20; // 20% mais caro
+    @Override
+    public String getTipoVaga() {
+        return "VIP";
     }
 }
