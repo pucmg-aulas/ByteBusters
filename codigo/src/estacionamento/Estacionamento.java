7c737402132cfef328ec868f61ad86a7a43bb860
Kayque Allan <164039275+kayqueallan@users.noreply.github.com>
2024-10-20 18:37:36 -0300

    Update Estacionamento.java

--- codigo/src/estacionamento/Estacionamento.java
+++ codigo/src/estacionamento/Estacionamento.java
@@ -1,6 +1,7 @@
 package estacionamento;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Estacionamento {
     private String nome;
@@ -11,6 +12,44 @@ public class Estacionamento {
         this.nome = nome;
         this.endereco = endereco;
         this.vagas = new ArrayList<>();
+        inicializarVagas(); // Chama o método para inicializar as vagas
+    }
+
+    private void inicializarVagas() {
+        // Criar 10 Vagas Normais
+        for (int i = 1; i <= 10; i++) {
+            vagas.add(new Normal("A", i));
+        }
+
+        // Criar 5 Vagas VIP
+        for (int i = 1; i <= 5; i++) {
+            vagas.add(new Vip("B", i));
+        }
+
+        // Criar 5 Vagas PCD
+        for (int i = 1; i <= 5; i++) {
+            vagas.add(new Pcd("C", i));
+        }
+
+        // Criar 5 Vagas Idoso
+        for (int i = 1; i <= 5; i++) {
+            vagas.add(new Idoso("D", i));
+        }
+    }
+
+    public void mostrarEstacionamento() {
+        System.out.println("Nome do Estacionamento: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Total de Vagas: " + vagas.size());
+        System.out.println("Vagas Ocupadas: " + (vagas.size() - (int) vagas.stream().filter(Vaga::estaDisponivel).count()));
+    }
+
+    public void mostrarVagas() {
+        for (Vaga vaga : vagas) {
+            System.out.println("ID da Vaga: " + vaga.getId() +
+                    ", Tipo: " + vaga.getTipoVaga() +
+                    ", Disponível: " + vaga.estaDisponivel());
+        }
     }
 
     public void adicionarVaga(Vaga vaga) {
@@ -20,17 +59,4 @@ public class Estacionamento {
     public List<Vaga> getVagas() {
         return vagas;
     }
-
-    public void mostrarVagas() {
-        System.out.println("\nVagas no Estacionamento " + nome + ":");
-        for (Vaga vaga : vagas) {
-            System.out.println("Vaga "+vaga.getTipoVaga()+" ID: " + vaga.getId() + " - Disponível: " + (vaga.estaDisponivel() ? "Sim" : "Não"));
-        }
-    }
-
-    public void mostrarEstacionamento() {
-        System.out.println("\nNome do Estacionamento: " + nome + ", Endereço: " + endereco + ", Quantidade de Vagas: " + vagas.size());
-    }
 }
-
-
