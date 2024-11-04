e4c1ec801d7d440f6cff2208f8525b4ce3094a5b
najarhenrique <105832522+najarhenrique@users.noreply.github.com>
2024-11-03 21:16:35 -0300

    Update Vaga.java

--- codigo/src/model/Vaga.java
+++ codigo/src/model/Vaga.java
@@ -21,31 +21,3 @@ public abstract class Vaga {
 
     public abstract double getFatorPreco();
 }
-
-class VagaPcd extends Vaga {
-    @Override
-    public double getFatorPreco() {
-        return 0.8;  // 20% de desconto
-    }
-}
-
-class VagaIdoso extends Vaga {
-    @Override
-    public double getFatorPreco() {
-        return 0.9;  // 10% de desconto
-    }
-}
-
-class VagaVip extends Vaga {
-    @Override
-    public double getFatorPreco() {
-        return 1.2;  // 20% de acréscimo
-    }
-}
-
-class VagaNormal extends Vaga {
-    @Override
-    public double getFatorPreco() {
-        return 1.0;  // Sem desconto ou acréscimo
-    }
-}
